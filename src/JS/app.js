023abf128ded609a049de3e0c65a28f53f6202e4
Ryan E. Wilson <ryanwilson2005@gmail.com>
2023-03-01 03:27:16 -0500

    Test JS functionality. Add styles and JS code

--- src/JS/app.js
+++ src/JS/app.js
@@ -1,1 +1,23 @@
-const firstColumn = document.querySelector("col order-first");
+// const firstColumn = document.querySelector(".col.order-first");
+// const secondColumn = document.querySelector(".col.order-2");
+// const thirdColumn = document.querySelector(".col.order-3");
+const allColumns = document.querySelectorAll(
+  ".col.order-first, .col.order-2, .col.order-last"
+);
+
+function toggleClassOnMouseOver(element, className) {
+  element.addEventListener("mouseover", () => {
+    element.classList.add(className);
+  });
+
+  element.addEventListener("mouseout", () => {
+    element.classList.remove(className);
+  });
+}
+
+allColumns.forEach((column) => {
+  toggleClassOnMouseOver(column, "containerRev");
+});
+
+
+
